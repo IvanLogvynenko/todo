f664b3966759316edda4fdb290cbdfa09db57681
IvanLogvynenko <ivanlogvynenko2112@gmail.com>
2025-05-01 17:03:42 +0200

    update container

--- server/Test/main.go
+++ server/Test/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/IvanLogvynenko/todo/server/test/structs"
@@ -12,13 +13,16 @@ func main() {
 	stored_data := 0
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
+			log.Println("Got new GET request")
 			responce := structs.Responce{
 				Value: "value",
 				Test:  stored_data,
 			}
 			w.WriteHeader(http.StatusAccepted)
 			json.NewEncoder(w).Encode(responce)
+			return
 		} else if r.Method == "POST" {
+			log.Println("Got new POST request")
 			var request structs.Request
 			err := json.NewDecoder(r.Body).Decode(&request)
 			if err != nil {
@@ -26,12 +30,17 @@ func main() {
 				fmt.Fprintln(w, "Corrupted json")
 				return
 			}
+			stored_data = request.Test
 			w.WriteHeader(http.StatusOK)
 			responce := structs.Responce{
 				Value: "value",
 				Test:  stored_data,
 			}
 			json.NewEncoder(w).Encode(responce)
+			return
 		}
+		log.Println("500(((")
 	})
+	fmt.Println("Starting server on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
